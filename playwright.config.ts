3cc27135a02975905ae30a94a865b7598227dc8a
Mateusz Sańko <mateusz.sanko@gmail.com>
2024-02-03 22:06:46 +0100

    Change assertion timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,9 @@ import {defineConfig, devices} from '@playwright/test';
  */
 export default defineConfig({
     timeout: 30 * 1000,
+    expect: {
+        timeout: 10 * 1000
+    },
     testDir: './tests',
     /* Run tests in files in parallel */
     fullyParallel: true,
